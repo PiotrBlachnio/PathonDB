0516d92583fc6a1110fce966316cae712e6041fc
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-08-24 21:14:25 +0200

    Initialize repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Json_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
