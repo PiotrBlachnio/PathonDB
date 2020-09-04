e54333046ea2ec655ea8697741b331c937f97c13
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-09-04 06:55:32 +0200

    Create IDatabase interface

--- Models/IDatabase.cs
+++ Models/IDatabase.cs
@@ -1,1 +1,9 @@
-// name
+namespace JsonDatabase.Models {
+    public interface IDatabase {
+        string[] GetTableNames();
+
+        Table GetTable(string tableName);
+
+        void AddTable(Table table);
+    }
+}
