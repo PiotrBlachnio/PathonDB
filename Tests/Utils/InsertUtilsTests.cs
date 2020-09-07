94637c42bd0ce30795c6378616214671c2c6b1e9
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-09-07 06:43:12 +0200

    Add tests to GetTableNameFromQuery method in InsertUtils

--- Tests/Utils/InsertUtilsTests.cs
+++ Tests/Utils/InsertUtilsTests.cs
@@ -5,12 +5,23 @@ namespace JsonDatabase.Tests.Utils {
     public class InsertUtilsTests {
         [Theory]
         [InlineData("INSERT INTO users (email, phoneNumber) VALUES (\"Jeff@gmail.com\", 703503);")]
-        [InlineData("INSERT INTO    users   (email, phoneNumber)   VALUES    (\"Jeff@gmail.com\", 703503);")]
-        [InlineData("INSERT INTO users (email, phoneNumber) VALUES (\"Jeff@gmail.com\", 703503);")]
-        public void GetArgumentsFromQuery_ShouldReturnVaidStringArray(string query) {
+        [InlineData("INSERT INTO users   (email, phoneNumber) VALUES   (\"Jeff@gmail.com\", 703503);")]
+        public void GetArgumentsFromQuery_ShouldReturnValidStringArray(string query) {
             var actual = InsertUtils.GetArgumentsFromQuery(query);
             
             var expected = new string[3] { "INSERT INTO users", "email, phoneNumber) VALUES", "\"Jeff@gmail.com\", 703503);"};
+
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("INSERT INTO users (email, phoneNumber) VALUES (\"Jeff@gmail.com\", 703503);")]
+        [InlineData("INSERT INTO USErs (email, phoneNumber) VALUES (\"Jeff@gmail.com\", 703503);")]
+        [InlineData("INSERT INTO   usErs    (email, phoneNumber) VALUES (\"Jeff@gmail.com\", 703503);")]
+        public void GetTableNameFromQuery_ShouldReturnValidTableName(string query) {
+            var actual = InsertUtils.GetTableNameFromQuery(query);
+
+            var expected = "users";
 
             Assert.Equal(actual, expected);
         }
