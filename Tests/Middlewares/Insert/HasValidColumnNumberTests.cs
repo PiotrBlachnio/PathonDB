51b1e93f86eebba190c783e68647b8952c64af46
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-09-09 17:52:21 +0200

    Remove testing error messages from insert middlewares

--- Tests/Middlewares/Insert/HasValidColumnNumberTests.cs
+++ Tests/Middlewares/Insert/HasValidColumnNumberTests.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using Autofac.Extras.Moq;
 using JsonDatabase.Exceptions.Insert;
 using JsonDatabase.Middlewares.Insert;
 using JsonDatabase.Models;
-using Moq;
 using Xunit;
 
 namespace JsonDatabase.Tests.Middlewares.Insert {
@@ -25,19 +23,17 @@ namespace JsonDatabase.Tests.Middlewares.Insert {
         }
         
         [Theory]
-        [InlineData("INSERT INTO users (email) VALUES (\"Jeff@gmail.com\", 703503);", 1)]
-        [InlineData("INSERT INTO users (   ) VALUES (\"Jeff@gmail.com\", 703503);", 0)]
-        [InlineData("INSERT INTO users (email, username, password) VALUES (\"Jeff@gmail.com\", 703503);", 3)]
-        [InlineData("INSERT INTO users (email, username,  ) VALUES (\"Jeff@gmail.com\", 703503);", 3)]
-        public void Check_InvalidColumnNumber_ShouldThrowInvalidColumnNumberException(string query, int actualColumnNumber) {
+        [InlineData("INSERT INTO users (email) VALUES (\"Jeff@gmail.com\", 703503);")]
+        [InlineData("INSERT INTO users (   ) VALUES (\"Jeff@gmail.com\", 703503);")]
+        [InlineData("INSERT INTO users (email, username, password) VALUES (\"Jeff@gmail.com\", 703503);")]
+        [InlineData("INSERT INTO users (email, username,  ) VALUES (\"Jeff@gmail.com\", 703503);")]
+        public void Check_InvalidColumnNumber_ShouldThrowInvalidColumnNumberException(string query) {
             using (var mock = AutoMock.GetLoose()) {
                 mock.Mock<IDatabase>().Setup(m => m.GetTable("users")).Returns(new MockedTable());
 
                 var middleware = mock.Create<HasValidColumnNumber>();
 
-                var ex = Assert.Throws<InvalidColumnNumberException>(() => middleware.Check(query));
-
-                Assert.Equal($"Expected column number: 2, received: {actualColumnNumber}", ex.Message);
+                Assert.Throws<InvalidColumnNumberException>(() => middleware.Check(query));
             }
         }
     }
