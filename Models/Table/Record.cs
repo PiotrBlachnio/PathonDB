66663e9635935dc3c5d80e07a396f7130a504441
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-09-26 11:43:13 +0200

    Update record model

--- Models/Table/Record.cs
+++ Models/Table/Record.cs
@@ -1,12 +1,22 @@
+using System.Collections.Generic;
+
 namespace PathonDB.Models.Table {
     public class Record {
         public string Id { get; set; }
-        public string[] ColumnNames { get; private set; }
-        public string[] Values { get; private set; }
+        public IList<string> ColumnNames { get; private set; }
+        public IList<string> Values { get; private set; }
 
-        public Record(string[] columnNames, string[] values) {
-            this.ColumnNames = columnNames;
-            this.Values = values;
+        public Record(string[] columnNames = null, string[] values = null) {
+            this.ColumnNames = columnNames == null ? new string[] {} : columnNames;
+            this.Values = values == null ? new string[] {} : values;
+        }
+
+        public void AddColumnName(string columnName) {
+            this.ColumnNames.Add(columnName);
+        }
+
+        public void AddValue(string value) {
+            this.Values.Add(value);
         }
     }
 }
