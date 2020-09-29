ab1f32c22f13542452fcb0b5c0a6e3129ac16c59
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-09-29 06:51:33 +0200

    Add tests to Check_InvalidColumns method in HasValidColumns select middleware

--- Tests/Middlewares/Select/HasValidColumnsTests.cs
+++ Tests/Middlewares/Select/HasValidColumnsTests.cs
@@ -1,4 +1,5 @@
 using Autofac.Extras.Moq;
+using PathonDB.Exceptions.General;
 using PathonDB.Middlewares.Select;
 using PathonDB.Models.Database;
 using Xunit;
@@ -21,6 +22,22 @@ namespace PathonDB.Tests.Middlewares.Select {
                 var actual = middleware.Check(query);
 
                 Assert.True(actual);
+            }
+        }
+
+        [Theory]
+        [InlineData("   SELECT   (emaill)   FROM    users    ")]
+        [InlineData("SELECT (Id, email, username) FROM users")]
+        [InlineData("   SELECT   (isAdult, _id)   FroM    users  ;  ")]
+        [InlineData("SELECT (isAdult,   id  ,   phoneNumber, IDD) FROM users WHERE isAdult=true")]
+        [InlineData("   SELECT   (   isAdult  ,    id   ,   phoneNumberr   )   FROM    users  wheRe    isAdult    = true    ")]
+        public void Check_InvalidColumns_ShouldThrowUnknownColumnNameException(string query) {
+            using (var mock = AutoMock.GetLoose()) {
+                mock.Mock<IDatabase>().Setup(m => m.GetTable("users")).Returns(new MockedTable());
+
+                var middleware = mock.Create<HasValidColumns>();
+
+                Assert.Throws<UnknownColumnNameException>(() => middleware.Check(query));
             }
         }
     }
