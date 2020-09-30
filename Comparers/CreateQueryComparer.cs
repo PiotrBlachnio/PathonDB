e1b191c79d264fa1ab0b786dbf92c22f9f739892
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-09-30 16:46:24 +0200

    Rename ColumnComparer to CreateQueryComparer

--- Comparers/CreateQueryComparer.cs
+++ Comparers/CreateQueryComparer.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace PathonDB.Comparers {
-    public class ColumnComparer : IEqualityComparer<string[]> {
+    public class CreateQueryComparer : IEqualityComparer<string[]> {
         public bool Equals(string[] firstColumn, string[] secondColumn) {
             return firstColumn[0].ToLower() == secondColumn[0].ToLower();
         }
