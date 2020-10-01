a405c63b87a1c22b5c00bc33b5a9dc142a2cbfe8
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-01 06:29:38 +0200

    Create Startup class

--- server/Program.cs
+++ server/Program.cs
@@ -1,17 +1,42 @@
-﻿using PathonDB.Models.Database;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace PathonDB {
-    class Program {
-        static void Main(string[] args) {
-            var client = new Client();
-            var queries = new string[] {
-                "  CREATE   TABLE   users    (  email   text   , phoneNumber int, isAdult boolean, email text);",
-                "INSERT    INTO   users    (isAdult,  email,  phoneNumber )  VALUES  (false, \"Jeff@gmail.com\", 703503);",
-                "INSERT    INTO   users    (isAdult,  email,  phoneNumber )  VALUES  (false, \"Arnold@gmail.com\", 141505);",
-                "SELECT * FROM  userS WHERE isAdult = false;"
-            };
-
-            client.ExecuteQuery(queries); 
+    public class Program {
+        
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging((context, logging) => {
+                logging.ClearProviders();
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webBuilder => {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
+
+
+// using PathonDB.Models.Database;
+
+// namespace PathonDB {
+//     class Program {
+//         static void Main(string[] args) {
+//             var client = new Client();
+//             var queries = new string[] {
+//                 "  CREATE   TABLE   users    (  email   text   , phoneNumber int, isAdult boolean, email text);",
+//                 "INSERT    INTO   users    (isAdult,  email,  phoneNumber )  VALUES  (false, \"Jeff@gmail.com\", 703503);",
+//                 "INSERT    INTO   users    (isAdult,  email,  phoneNumber )  VALUES  (false, \"Arnold@gmail.com\", 141505);",
+//                 "SELECT * FROM  userS WHERE isAdult = false;"
+//             };
+
+//             client.ExecuteQuery(queries); 
+//         }
+//     }
+// }
---
+++ server/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PathonDB {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            // services.ConfigureDatabase(Configuration);
+
+            // services.ConfigureAutoMapper();
+
+            // services.ConfigureControllers();
+
+            // services.AddServices();
+
+            // services.ConfigureSwagger();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.Use((context, next) => {
+                context.Request.EnableBuffering();
+                return next();
+            });
+        }
+    }
+}
