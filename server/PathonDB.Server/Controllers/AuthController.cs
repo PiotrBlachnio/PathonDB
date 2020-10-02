c355074800a9e5e19c407e53e43214f92cd0173a
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-02 17:49:01 +0200

    Create AuthMiddleware

---
+++ server/PathonDB.Server/Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PathonDB.Server.Controllers {
+
+    [ApiController]
+    public class AuthController : ControllerBase {
+        
+    }
+}
---
+++ server/PathonDB.Server/Middlewares/AuthMiddleware.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PathonDB.Server.Middlewares {
+    public class AuthMiddleware {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<AuthMiddleware> _logger;
+
+        public AuthMiddleware(RequestDelegate next, ILogger<AuthMiddleware> logger) {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context) {
+            var databaseKey = GetCookie(context, "database-key");
+
+            if(!IsDatabaseKeyValid(databaseKey)) throw new Exception("Database key is invalid");
+            
+            await _next(context);
+        }
+
+        private string GetCookie(HttpContext context, string cookieName) {
+            context.Request.Cookies.TryGetValue("database-key", out string cookie);
+            return cookie;
+        }
+
+        private bool IsDatabaseKeyValid(string databaseKey) {
+            return Guid.TryParse(databaseKey, out Guid guid);
+        }
+    }
+}
