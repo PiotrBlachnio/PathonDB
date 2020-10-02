fe987936634e38a6ef8791b246f220d2fa890332
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-02 19:43:32 +0200

    Create GetNewDatabaseKey endpoint

--- server/PathonDB.Server/Controllers/AuthController.cs
+++ server/PathonDB.Server/Controllers/AuthController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using PathonDB.Server.Contracts;
+using PathonDB.Server.Contracts.Responses;
+using PathonDB.Server.Services;
 
 namespace PathonDB.Server.Controllers {
 
     [ApiController]
     public class AuthController : ControllerBase {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService) {
+            _authService = authService;
+        }
+
         [HttpGet(ApiRoutes.Auth.NewKey)]
         public ActionResult GetNewDatabaseKey() {
-            return Ok("Hi");
+            var key = _authService.GenerateDatabaseKey();
+            var response = new NewKeyResponse() { Key = key };
+
+            return Ok(response);
         }
     }
 }
