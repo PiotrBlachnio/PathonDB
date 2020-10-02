aacaecff631f4fb1c810623f89c0f77b536f880b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-02 20:13:07 +0200

    Refactor AuthMiddleware

--- server/PathonDB.Server/Middlewares/AuthMiddleware.cs
+++ server/PathonDB.Server/Middlewares/AuthMiddleware.cs
@@ -14,20 +14,20 @@ namespace PathonDB.Server.Middlewares {
         }
 
         public async Task InvokeAsync(HttpContext context) {
-            var databaseKey = GetCookie(context, "database-key");
-            if(!IsDatabaseKeyValid(databaseKey)) throw new Exception("Database key is invalid");
+            var key = GetKeyFromCookies(context);
+            if(!IsKeyValid(key)) throw new Exception("Access key is invalid");
             
-            _logger.LogInformation("Database key has passed authentication process successfully");
+            _logger.LogInformation("Access key has passed authentication process successfully");
             await _next(context);
         }
 
-        private string GetCookie(HttpContext context, string cookieName) {
-            context.Request.Cookies.TryGetValue("database-key", out string cookie);
+        private string GetKeyFromCookies(HttpContext context) {
+            context.Request.Cookies.TryGetValue("access-key", out string cookie);
             return cookie;
         }
 
-        private bool IsDatabaseKeyValid(string databaseKey) {
-            return Guid.TryParse(databaseKey, out Guid guid);
+        private bool IsKeyValid(string key) {
+            return Guid.TryParse(key, out Guid guid);
         }
     }
 }
