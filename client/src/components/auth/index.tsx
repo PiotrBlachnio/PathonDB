8e8b13a3c614c8a125d8bed7e8b97c6fc355e0e0
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-03 15:40:25 +0200

    Create paper style in auth component

--- client/src/components/auth/index.tsx
+++ client/src/components/auth/index.tsx
@@ -1,8 +1,21 @@
 import React, { ReactElement } from "react";
+import { Paper } from '@material-ui/core';
+
+const paperStyle = {
+    width: 700,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    height: 300,
+    border: '1px solid black'
+};
 
 const Auth: React.FC = (): ReactElement => {
     return (
-        <h1>Auth</h1>
+        <Paper elevation={3} style={paperStyle as any}>
+
+        </Paper>
     );
 }
 
