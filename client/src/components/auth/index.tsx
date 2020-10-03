ff079dfcef096321e5ac516fe681514d4b22cb82
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-03 16:29:23 +0200

    Refactor auth component

--- client/src/components/auth/index.tsx
+++ client/src/components/auth/index.tsx
@@ -1,36 +1,24 @@
-import React, { ReactElement } from "react";
-import { Paper, TextField, Button } from '@material-ui/core';
+import React, { ReactElement, useState } from "react";
+import Login from './Login';
+import Register from './Register';
 
-const paperStyle = {
-    width: 700,
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    height: 300
-};
-
-const inputStyle = {
-    position: 'relative',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 310
-};
-
-const buttonStyle = {
-    position: 'relative',
-    top: '70%',
-    transform: 'translateX(-25%)'
-};
+enum AuthPage {
+    LOGIN = 1,
+    REGISTER = 2
+}
 
 const Auth: React.FC = (): ReactElement => {
-    return (
-        <Paper elevation={24} style={paperStyle as any}>
-            <TextField variant='outlined' style={inputStyle as any} placeholder='xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' label='Access key'/>
-            <Button color='primary' variant='contained' style={buttonStyle as any}>Authenticate</Button>
-        </Paper>
-    );
+    const [currentPage, setCurrentPage] = useState<AuthPage>(AuthPage.LOGIN);
+
+    const renderPage = (): ReactElement => {
+        if(currentPage == AuthPage.LOGIN) {
+            return <Login />
+        } else {
+            return <Register />
+        }
+    }
+
+    return renderPage();
 }
 
 export default Auth;
