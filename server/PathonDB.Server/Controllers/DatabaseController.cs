4f4e3ab6da992c32a10c966a18557b8c0030ae82
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-03 06:16:31 +0200

    Update DatabaseController

--- server/PathonDB.Server/Controllers/DatabaseController.cs
+++ server/PathonDB.Server/Controllers/DatabaseController.cs
@@ -1,12 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PathonDB.DatabaseClient.Models.Database;
 using PathonDB.Server.Contracts;
+using PathonDB.Server.Contracts.Requests;
+using PathonDB.Server.Services;
 
 namespace PathonDB.Server.Controllers {
 
     [ApiController]
     public class DatabaseController : ControllerBase {
+        private readonly Dictionary<string, Client> _database = new Dictionary<string, Client>() ;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthService _authService;
+
+        public DatabaseController(IHttpContextAccessor httpContextAccessor, IAuthService authService) {
+            _httpContextAccessor = httpContextAccessor;
+            _authService = authService;
+        }
+        
         [HttpPost(ApiRoutes.Database.Query)]
-        public ActionResult PerformQuery() {
+        public ActionResult PerformQuery([FromBody] QueryRequest body) {
+            var key = _authService.GetKeyFromHttpContext(_httpContextAccessor.HttpContext);
+
+            if(_database.ContainsKey(key)) {
+                _database[key].ExecuteQuery(new string[] { body.Query });
+            }
             return Ok("Database controller");
         }
     }
