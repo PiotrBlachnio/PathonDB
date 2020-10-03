50b50d062681ab348a393247efe5d1a8ee0a211e
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-03 08:17:21 +0200

    Remove automapper

--- server/PathonDB.Server/Extensions/ServiceExtensions.cs
+++ server/PathonDB.Server/Extensions/ServiceExtensions.cs
@@ -1,5 +1,3 @@
-using System;
-using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
@@ -12,10 +10,6 @@ namespace PathonDB.Server.Extensions {
             services.AddControllers().AddNewtonsoftJson(s => {
                 s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             });
-        }
-
-        public static void ConfigureAutoMapper(this IServiceCollection services) {
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
         
         public static void AddServices(this IServiceCollection services) {
