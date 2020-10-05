a5ae7e29e9db61c5d307b28f971e9d2f3f863977
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-05 05:51:01 +0200

    Add styles to CopyButton component

--- client/src/components/auth/CopyButton.tsx
+++ client/src/components/auth/CopyButton.tsx
@@ -2,12 +2,16 @@ import React, { ReactElement, CSSProperties } from "react";
 import { FileCopy } from '@material-ui/icons';
 
 const style: CSSProperties = {
-
+    position: 'relative',
+    top: '50%',
+    left: '15%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: 22
 };
 
 const CopyButton: React.FC = (props): ReactElement => {
     return (
-        <FileCopy />
+        <FileCopy style={style} />
     );
 }
 
