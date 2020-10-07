81227d22fb211653f8adc0f6bf29b846b1af9c8d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-07 06:34:19 +0200

    Implement logout process on the client side

--- client/src/components/home/Navbar.tsx
+++ client/src/components/home/Navbar.tsx
@@ -1,19 +1,31 @@
 import React, { ReactElement, CSSProperties } from "react";
 import { AppBar, Toolbar, Button } from '@material-ui/core';
+import { logout } from '../../utils/api';
+import { useHistory } from "react-router-dom";
+import { IsAuthenticated, setIsAuthenticated } from '../../utils';
 
 const style: CSSProperties = {
     marginLeft: 'auto'
 };
 
 const Navbar: React.FC = (): ReactElement => {
-    const logout = (): void => {
+    const history = useHistory();
 
+    const handleButtonClick = async (): Promise<void> => {
+        try {
+            await logout();
+            setIsAuthenticated(IsAuthenticated.FALSE);
+
+            history.push('/auth');
+        } catch(error) {
+            console.error(error);
+        }
     };
 
     return (
         <AppBar>
             <Toolbar>
-                <Button color='inherit' style={style} onClick={logout}>Logout</Button>
+                <Button color='inherit' style={style} onClick={handleButtonClick}>Logout</Button>
             </Toolbar>
         </AppBar>
     );
--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const authorizeWithAccessKey = async (key: string): Promise<void> => {
     return await axios.post('/auth/existing-key', { key });
 };
+
+export const logout = async (): Promise<void> => {
+    return await axios.post('/auth/logout');
+};
