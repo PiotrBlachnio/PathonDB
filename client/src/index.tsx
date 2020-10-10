9faa0df5933c6cb26aecc156b5ef3ffbc21077e1
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-10 20:40:56 +0200

    Fix axios issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}/api/v1`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.withCredentials = true;
 
 ReactDOM.render(
   <React.StrictMode>
